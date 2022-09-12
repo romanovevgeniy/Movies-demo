5f734da39167316f89ea3d975dc0cdfaf942ca7e
eromanov <romanov.evgeniy86@gmail.com>
2022-09-12 19:04:42 +0300

    add link to the repository

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -6,7 +6,11 @@ const Footer = () => {
       <div className="footer-copyright deep-purple lighten-3">
         <div className="container">
           {new Date().getFullYear()} Copyright Text
-          <a className="grey-text text-lighten-4 right" href="#!">
+          <a
+            className="grey-text text-lighten-4 right"
+            href="https://github.com/romanovevgeniy/movies-demo"
+            target="_blank"
+          >
             Repo
           </a>
         </div>
