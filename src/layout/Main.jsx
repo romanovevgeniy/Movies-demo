cc35c68eea7077e8542a45c4beb3f8e426485885
eromanov <romanov.evgeniy86@gmail.com>
2022-09-12 09:17:09 +0300

    add component Movie and Movies

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import CardItem from "./CardItem";
 
 function Main() {
-  return <div className="container content">Main</div>;
+  return (
+    <div className="container content">
+      <CardItem />
+    </div>
+  );
 }
 
 export default Main;
